14cc83533a4d28e801e0f20254b64f950cdf7b4f
Hemant kumar <hemantkumarsonkar1@gmail.com>
2024-06-17 15:56:59 +0530

    done

--- src/app.js
+++ src/app.js
@@ -9,12 +9,14 @@ app.use(bodyParser.json());
 
 // Mock user data (Replace with actual user authentication logic)
 const users = [
-  // Define user objects here
+  { id: 1, username: 'user1', password: 'password1' },
+  { id: 2, username: 'user2', password: 'password2' },
 ];
 
 // Mock product data (Replace with actual product retrieval logic)
 const products = [
-  // Define product objects here
+  { id: 1, name: 'Product A', price: 10 },
+  { id: 2, name: 'Product B', price: 20 },
 ];
 
 // Authentication endpoint (Students should implement this)
@@ -22,8 +24,19 @@ app.post('/login', (req, res) => {
   // Implement user authentication logic here
   // If authentication is successful, generate a JWT token and send it in the response
   // Example token generation:
-  // const token = jwt.sign({ userId: user.id, username: user.username }, secretKey);
-  // res.status(201).json({ token });
+
+  const { username, password } = req.body;
+
+  const user = users.find((user) => user.username === username && user.password === password);
+
+  if(!user) {
+    res.status(401).json({
+      message: "Authentication failed",
+    })
+  }
+
+  const token = jwt.sign({ userId: user.id, username: user.username }, secretKey);
+  res.status(201).json({ token });
 });
 
 // Product route (Students should implement this)
@@ -32,7 +45,27 @@ app.get('/product', (req, res) => {
   // Implement JWT token verification logic here
   // If the token is valid, students can access product data and send it in the response
   // Example response:
-  // res.status(200).json({ message: 'Product data', products: productData });
+  
+
+  const token = req.headers?.authorization?.split(" ")[1];
+  console.log(token, "token");
+  if(!token) {
+    return res.status(401).json({
+      message: "Authentication Required",
+    });
+  }
+
+  jwt.verify(token, secretKey, (error, decoded) => {
+    if(error) {
+      return res.status(401).json({
+        message: "Invalid token " + error.message,
+      });
+    }
+
+    const productData = products;
+    res.status(200).json({ message: 'Product data', products: productData });
+  })
+
 });
 
 module.exports = app;
